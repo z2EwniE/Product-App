699163ea2008290c30446437b3159d80ea0376ce
calvskie <shadow>
2023-10-31 23:20:29 +0800

    calvskie

---
+++ app/src/main/java/com/example/foodcalculator/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.foodcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)q
+        setContentView(R.layout.activity_second)
+    }
+}
